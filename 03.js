74ac104fc4f8fe4772ca8cb560e1fdc3577e2331
tannousmarc <tannous.marc@gmail.com>
2020-12-03 07:21:17 +0200

    not me to mess up the X and Y axes, take 10 mins to spot it, and lose 2000 positions because of it

--- 03.js
+++ 03.js
@@ -4,15 +4,15 @@ fs.readFile('inputs/03.txt', 'utf8', (err, data) => {
     if(err) throw err;
     data = data.split('\n');
 
-    let pos = [0, 0];
+    let [xPos, yPos] = [0, 0];
     const yOffset = 2;
     const xOffset = 1;
     let res = 0;
 
-    while(pos[1] + yOffset < data.length){
-        pos[0] = pos[0] + xOffset;
-        pos[1] = pos[1] + yOffset;
-        if(data[pos[1]][pos[0]  % data[0].length] === '#')
+    while(yPos + yOffset < data.length){
+        xPos = xPos + xOffset;
+        yPos = yPos + yOffset;
+        if(data[yPos][xPos  % data[0].length] === '#')
             res++;
     }
 
