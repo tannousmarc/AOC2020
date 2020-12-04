c85f66762b18927dd7505ef43cb7a7aa942c5296
tannousmarc <tannous.marc@gmail.com>
2020-12-04 09:20:26 +0200

    regex magic

--- 04.js
+++ 04.js
@@ -9,67 +9,48 @@ fs.readFile('inputs/04.txt', 'utf8', (err, data) => {
         passport = passport.split(/\n| /);
 
         const required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
-
         let validFields = 0;
+        const removeAndIncrement = () => {
+            required.splice(required.indexOf(field.substring(0, 3)), 1);
+            validFields++;
+        }
+
         for(field of passport){
             const key = required[required.indexOf(field.substring(0, 3))];
 
             if(key !== -1){
-                let val = field.substring(4);
+                const val = field.substring(4);
 
                 switch(key) {
                     case 'byr':
-                        if(Number(val) >= 1920 && Number(val) <= 2002 && val.length === 4){
-                            required.splice(required.indexOf(field.substring(0, 3)), 1);
-                            validFields++;
-                        }
+                        if(/^19[2-9][0-9]$|^200[0-2]$/.test(val))
+                            removeAndIncrement();
                         break;
                     case 'iyr':
-                        if(Number(val) >= 2010 && Number(val) <= 2020 && val.length === 4){
-                            required.splice(required.indexOf(field.substring(0, 3)), 1);
-                            validFields++;
-                        }
+                        if(/^201[0-9]$|^2020$/.test(val))
+                            removeAndIncrement();
                         break;
                     case 'eyr':
-                        if(Number(val) >= 2020 && Number(val) <= 2030 && val.length === 4){
-                            required.splice(required.indexOf(field.substring(0, 3)), 1);
-                            validFields++;
-                        }
+                        if(/^202[0-9]$|^2030$/.test(val))
+                            removeAndIncrement();
                         break;
                     case 'hgt':
-                        const standard = field.slice(-2);
-                        val = val.substring(0, val.length - 2);
-                        if(standard === 'cm' && Number(val) >= 150 && Number(val) <= 193){
-                            required.splice(required.indexOf(field.substring(0, 3)), 1);
-                            validFields++;
-                        }
-                        else if(standard === 'in' && Number(val) >= 59 && Number(val) <= 76){
-                            required.splice(required.indexOf(field.substring(0, 3)), 1);
-                            validFields++;
-                        }
+                        if(/^(1[5-8][0-9]|19[0-3])cm$|^(59|6[0-9]|7[0-6])in$/.test(val))
+                            removeAndIncrement();
                         break;
                     case 'hcl':
-                        const first = val.substring(0, 1);
-                        const rest = val.substring(1);
-                        if(first === '#' && /[a-f0-9]{6}/.test(rest)){
-                            required.splice(required.indexOf(field.substring(0, 3)), 1);
-                            validFields++;
-                        }
+                        if(/^#[a-f0-9]{6}$/.test(val))
+                            removeAndIncrement();
                         break;
                     case 'ecl':
-                        const valid = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'];
-                        if(valid.indexOf(val) !== -1){
-                            required.splice(required.indexOf(field.substring(0, 3)), 1);
-                            validFields++;
-                        }
+                        if(/^amb$|^blu$|^brn$|^gry$|^grn$|^hzl$|^oth$/.test(val))
+                            removeAndIncrement();
                         break;
                     case 'pid':
-                        if(parseInt(val, 10) <= 999999999 && val.length === 9){
-                            required.splice(required.indexOf(field.substring(0, 3)), 1);
-                            validFields++;
-                        }
+                        if(/^[0-9]{9}$/.test(val))
+                            removeAndIncrement();
                         break;
-                    }
+                }
             }
         }
         if(validFields === 7)
