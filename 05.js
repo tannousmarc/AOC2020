1f5db1272bf79c00695416fc4fd472d685560dbd
tannousmarc <tannous.marc@gmail.com>
2020-12-05 07:24:03 +0200

    fun day, too bad I multiplied by 44 originally instead of 8; should make top 100 at one point if the stars align

---
+++ 05.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+fs.readFile('inputs/05.txt', 'utf8', (err, data) => {
+    if(err) throw err;
+    data = data.split('\n');
+
+    let seats = [];
+    for(ticket of data){
+        const vertical = parseInt(ticket.substring(0,7).split('').map(letter => letter === 'F' ? '0' : 1).join(""), 2);
+        const horizontal = parseInt(ticket.substring(7).split('').map(letter => letter === 'L' ? '0' : 1).join(""), 2);
+        seats.push(8 * vertical + horizontal);
+    }
+    seats = seats.sort(function(a, b) {
+        return a - b;
+    });
+
+    // figure it out from output: discard outliers at each end, in the middle there should be (a, b) with b = a + 2. Result is a + 1.
+    for(let i = 1; i < seats.length; i++){
+        if(seats[i] === seats[i-1] + 1 && seats[i] === seats[i+1] -1)
+            continue;
+        console.log(seats[i]);
+    }
+});
