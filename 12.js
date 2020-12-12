0e431c6e55a3df57d3c4c842cf7ec450e2ef0d0f
tannousmarc <tannous.marc@gmail.com>
2020-12-12 07:48:20 +0200

    🤮

---
+++ 12.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+
+fs.readFile('inputs/12.txt', 'utf8', (err, data) => {
+    if(err) throw err;
+    data = data.split('\n');
+
+    const dirs = ["E", "S", "W", "N"];
+
+    let facing = "E";
+    let pos = [0, 0];
+    let wPos = [10, 1];
+    for(elem of data){
+        switch (elem[0]){
+            case "N":
+                wPos[1] += Number(elem.substring(1));
+                break;
+            case "S":
+                wPos[1] -= Number(elem.substring(1));
+                break;
+            case "E":
+                wPos[0] += Number(elem.substring(1));
+                break;
+            case "W":
+                wPos[0] -= Number(elem.substring(1));
+                break;
+            case "R":
+                for(let i = 0; i < Number(elem.substring(1)) / 90; i++)
+                    wPos = [wPos[1], -wPos[0]];
+                break;
+            case "L":
+                for(let i = 0; i < Number(elem.substring(1)) / 90; i++)
+                    wPos = [-wPos[1], wPos[0]];
+                break;
+            case "F":
+                pos[1] += Number(elem.substring(1)) * wPos[1];
+                pos[0] += Number(elem.substring(1)) * wPos[0];
+            break;
+        }
+    }
+    console.log(pos);
+});
