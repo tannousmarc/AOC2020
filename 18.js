62e2903e44d789399be28fd2c56818bac136bcb0
tannousmarc <tannous.marc@gmail.com>
2020-12-18 11:15:09 +0200

    guess where .push() adds elements in JS

--- 18.js
+++ 18.js
@@ -8,42 +8,40 @@ fs.readFile('inputs/18.txt', 'utf8', (err, data) => {
     let sum = 0;
     for(elem of data){
         elem = elem.split(" ").join("");
+
         let vals = [];
         let ops = [];
+
+        const step = () => {
+            const operator = ops[0];
+            ops.splice(0, 1);
+            const [a, b] = [vals[0], vals[1]];
+            vals.splice(0, 2);
+            vals.unshift(operator === '+' ? a + b : a * b);
+        }
+
         for(tok of elem){
             if(tok >= '0' && tok <= '9')
-                vals.unshift(tok);
+                vals.unshift(Number(tok));
             else if(tok === '(')
                 ops.unshift(tok);
             else if(tok === ')'){
-                while(ops[0] !== '('){
-                    let operator = ops[0];
-                    ops.splice(0, 1);
-                    let [a, b] = [vals[0], vals[1]];
-                    vals.splice(0, 2);
-                    vals.unshift(operator === '+' ? Number(a) + Number(b) : Number(a) * Number(b));
-                }
+                while(ops[0] !== '(')
+                    step();
                 ops.splice(0, 1);
             }
             else{
-                while(ops.length > 0 && ops[0] !== '(' && !(ops[0] === '*' && tok === '+')){
-                    let operator = ops[0];
-                    ops.splice(0, 1);
-                    let [a, b] = [vals[0], vals[1]];
-                    vals.splice(0, 2);
-                    vals.unshift(operator === '+' ? Number(a) + Number(b) : Number(a) * Number(b));
-                }
+                while(ops.length > 0 && ops[0] !== '(' && !(ops[0] === '*' && tok === '+'))
+                    step();
                 ops.unshift(tok);
             }
         }
-        while(ops.length > 0){
-            let operator = ops[0];
-            ops.splice(0, 1);
-            let [a, b] = [vals[0], vals[1]];
-            vals.splice(0, 2);
-            vals.unshift(operator === '+' ? Number(a) + Number(b) : Number(a) * Number(b));
-        }
+
+        while(ops.length > 0)
+            step();
+
         sum += vals[0];
     }
+
     console.log(sum);
 });
