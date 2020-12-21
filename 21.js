8c0ee85bad543c3b70ddb41ae937d7a218a69f05
tannousmarc <tannous.marc@gmail.com>
2020-12-21 10:18:58 +0200

    we had this exact problem a few days ago??

--- 21.js
+++ 21.js
@@ -7,35 +7,29 @@ fs.readFile('inputs/21.txt', 'utf8', (err, data) => {
     
     let map = new Map();
     for(elem of data){
-        elem = elem.split("").filter(char => char !== '(' && char !== ')').join("");
+        elem = elem.replace(/\(|\)/g, "");
         elem = elem.split(/ |, /);
         let ingredients = new Set(elem.slice(0, elem.indexOf("contains")));
         let allergens = elem.slice(elem.indexOf("contains") + 1);
 
-        allergens.forEach(allergen => {
-            if(map.has(allergen))
-                map.set(allergen, new Set([...map.get(allergen)].filter(x => ingredients.has(x))))
-            else
-                map.set(allergen, ingredients);
-        });
+        allergens.forEach(allergen => map.set(allergen, map.has(allergen) ? new Set([...map.get(allergen)].filter(x => ingredients.has(x))) : ingredients));
     }
     
     const allergensNo = map.size;
     let found = 0;
     let realMap = new Map();
-    while(found < allergensNo){
-        for([key, val] of map){
+
+    while(found < allergensNo)
+        for([key, val] of map)
             if(val.size === 1){
                 const ingredient = val.values().next().value;
                 realMap.set(ingredient, key);
+                map.delete(key);
                 found++;
-                for([key2, val2] of map)
+                for([_, val2] of map)
                     if(val2.has(ingredient))
                         val2.delete(ingredient);
-                map.delete(key);
             }
-        }
-    }
     
     console.log(realMap);
 });
